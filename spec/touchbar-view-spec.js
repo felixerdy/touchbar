55e560b79d6b6ef97524d2d402db9421a1970801
felixerdy <felix.erdmann@uni-muenster.de>
2017-06-22 20:29:48 +0200

    hello touchbar 👆

---
+++ spec/touchbar-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TouchbarView from '../lib/touchbar-view';
+
+describe('TouchbarView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
